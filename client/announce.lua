eec71a26b0c3e776cdc1b986384d1ff86d10dc76
Cjneilson12 <96008403+Cjneilson12@users.noreply.github.com>
2022-04-07 16:43:58 +0100

    Delete client directory

--- client/announce.lua
+++
@@ -1,8 +0,0 @@
-RegisterCommand("announce", function(source, args)
-    TriggerServerEvent("announce", table.concat(args, " "))
-end)
-
-RegisterNetEvent("no-perms")
-AddEventHandler("no-perms", function()
-    TriggerEvent("chatMessage", "[Error]", {255,0,0}, "Sorry, but you don't have permission to do this" )
-end)
